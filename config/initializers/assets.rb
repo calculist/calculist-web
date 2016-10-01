e3ea88479538c43ffa83f6d01de58d5c2d3edf94
Dan Allison <daniel.j.allison@gmail.com>
2016-09-30 19:21:38 -0700

    Move fonts to vendor directory

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( worker.js )
+Rails.application.config.assets.precompile += ['worker.js', Proc.new { |path| true if path =~ /\.(eot|svg|ttf|woff)\z/ }]
