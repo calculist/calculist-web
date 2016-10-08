116b1ebf082a116980e12cc6f25955796ea1786c
Dan Allison <daniel.j.allison@gmail.com>
2016-10-08 16:08:15 -0700

    Use descriptive names

--- app/assets/javascripts/shared/utility/evalculist.js
+++ app/assets/javascripts/shared/utility/evalculist.js
@@ -11,17 +11,17 @@ lm.register('evalculist', [], function () {
     .split(/(".*?")/g).map(function (dqChunk) {
       return dqChunk.split(/('.*?')/g).map(function (sqChunk) {
         var isStr = sqChunk[0]  == '"' || sqChunk[0] == "'";
-        return isStr ? sqChunk : sqChunk.replace(/[a-zA-Z\d_\$]+/g, function(a,b){return isDigit(a[0]) ? a : 'c("' + a + '")';});
+        return isStr ? sqChunk : sqChunk.replace(/[a-zA-Z\d_\$]+/g, function(a,b){return isDigit(a[0]) ? a : '_var("' + a + '")';});
       }).join('');
     }).join('')
-    .replace(/c\("((?:\w|\$)+)"\)\[(.+?)\]/g, function(s, variableName, attributeName) {
-      return 'a(c("' + variableName + '"), ' + attributeName + ')';
+    .replace(/_var\("((?:\w|\$)+)"\)\[(.+?)\]/g, function(s, variableName, attributeName) {
+      return '_acc(_var("' + variableName + '"), ' + attributeName + ')';
     })
     .replace(ESCAPED_DOUBLE_QUOTES_PATTERN, '\\"')
     .replace(ESCAPED_SINGLE_QUOTES_PATTERN, "\\'");
 
     if (handlers === true) return string;
-    var fn = new Function('c','a', "'use strict';return " + string + ";");
+    var fn = new Function('_var','_acc', "'use strict';return " + string + ";");
     if (handlers) {
       return fn(handlers.variable, handlers.accessor);
     }
