a4e8c91900ac5483c0df227f3b89847ec2f4dbf9
Dan Allison <daniel.j.allison@gmail.com>
2016-12-12 19:51:38 -0800

    New move commands

---
+++ app/assets/javascripts/shared/commands/commands.moveToBottom.js
@@ -0,0 +1,10 @@
+calculist.register('commands.moveToBottom', ['_'], function (_) {
+
+  return function (item) {
+    _.pull(item.$parent.$items, item);
+    item.$parent.$items.push(item);
+    item.refreshSortOrder();
+    item.$parent.renderChildren();
+  }
+
+});
