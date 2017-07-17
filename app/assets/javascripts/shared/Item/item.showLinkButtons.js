5d618178b002961c809f089afa29a39724ac4fda
Dan Allison <daniel.j.allison@gmail.com>
2017-07-16 21:09:24 -0700

    Remove link buttons properly

--- app/assets/javascripts/shared/Item/item.showLinkButtons.js
+++ app/assets/javascripts/shared/Item/item.showLinkButtons.js
@@ -2,12 +2,22 @@ calculist.register('item.showLinkButtons', ['_','$','urlFinder','itemOfFocus'], 
 
   var externalLinkIcon = '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAVklEQVR4Xn3PgQkAMQhDUXfqTu7kTtkpd5RA8AInfArtQ2iRXFWT2QedAfttj2FsPIOE1eCOlEuoWWjgzYaB/IkeGOrxXhqB+uA9Bfcm0lAZuh+YIeAD+cAqSz4kCMUAAAAASUVORK5CYII=">';
 
+  var removeLinkButtons = function (_this) {
+    if (_this.$linkButtons) {
+      _this.$linkButtons.remove();
+      _this.$linkButtons = null;
+    }
+  };
+
   return function () {
     if (itemOfFocus.is(this)) {
       this.valueOf();
       var text = this.key + ' ' + (this.val || '');
       var urls = urlFinder.getUrls(text);
-      if (!urls || !urls.length) return;
+      if (!urls || !urls.length) {
+        removeLinkButtons(this);
+        return;
+      }
       if (!this.$linkButtons) {
         this.$linkButtons = $('<div class="link-buttons"></div>');
         this.$el.prepend(this.$linkButtons);
@@ -18,8 +28,7 @@ calculist.register('item.showLinkButtons', ['_','$','urlFinder','itemOfFocus'], 
         }).join('')
       );
     } else if (this.$linkButtons) {
-      this.$linkButtons.remove();
-      this.$linkButtons = null;
+      removeLinkButtons(this);
     }
   };
 });
