844e4029b7e817d75961985fe55dc150b3f1a59d
Dan Allison <daniel.j.allison@gmail.com>
2017-07-29 11:49:51 -0700

    Hide links that are not in the current view

--- app/assets/javascripts/shared/Item/item.showLinkButtons.js
+++ app/assets/javascripts/shared/Item/item.showLinkButtons.js
@@ -1,4 +1,4 @@
-calculist.register('item.showLinkButtons', ['_','$','urlFinder','itemOfFocus'], function (_, $, urlFinder, itemOfFocus) {
+calculist.register('item.showLinkButtons', ['_','$','urlFinder','itemOfFocus','zoomPage'], function (_, $, urlFinder, itemOfFocus, zoomPage) {
 
   var externalLinkIcon = '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAVklEQVR4Xn3PgQkAMQhDUXfqTu7kTtkpd5RA8AInfArtQ2iRXFWT2QedAfttj2FsPIOE1eCOlEuoWWjgzYaB/IkeGOrxXhqB+uA9Bfcm0lAZuh+YIeAD+cAqSz4kCMUAAAAASUVORK5CYII=">';
 
@@ -13,7 +13,7 @@ calculist.register('item.showLinkButtons', ['_','$','urlFinder','itemOfFocus'], 
     this.valueOf();
     var text = this.key + ' ' + (this.val || '');
     var urls = urlFinder.getUrls(text);
-    if (!urls || !urls.length) {
+    if (!urls || !urls.length || !zoomPage.isInPage(this)) {
       removeLinkButtons(this);
       return;
     }
