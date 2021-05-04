ead2f83683a616fc08b0641cf418e59f13f12b31
Dan Allison <daniel.j.allison@gmail.com>
2021-05-04 15:00:35 -0700

    Change displayed version back to v0.5.0

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,6 @@ module Calculist
     config.autoload_paths += %W(#{config.root}/app/services)
 
     # Corresponds to GitHub releases: https://github.com/calculist/calculist-web/releases
-    config.calculist_version = 'v1.0.0'
+    config.calculist_version = 'v0.5.0'
   end
 end
